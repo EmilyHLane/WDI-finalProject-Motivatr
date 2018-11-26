8c77d53af1581649fc3cabac8ebc60823893ba11
EMILYHLANE <embem80@gmail.com>
2018-11-26 15:54:02 -0800

    adds authenticated login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,9 @@
+const dotenv = require("dotenv");
+dotenv.config();
 const db = require("../models");
 const User = db.User;
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 //business logic - CRUD - here
 //find all users
@@ -21,25 +24,39 @@ const show = (req, res) => {
 
 //find user by email and password
 const login = (req, res) => {
+  //check for existing email
   User.findOne({ email: req.body.email })
     .exec()
     .then(function(user) {
+      //check encrypted password match
       bcrypt.compare(req.body.password, user.password, (err, result) => {
         if (err) {
           return res.status(401).json({
             failed: "unauthorized access"
           });
         }
+        //if match, create and send jwt token
         if (result) {
-          return res.status(200).json({
-            success: "welcome to jwt auth"
+          const payload = { id: user.id };
+          const secret = process.env.SECRET_OR_KEY;
+          jwt.sign(
+            payload,
+            secret,
+            // { expiresIn: "1h" },
+            (err, token) => {
+              console.log(token);
+              res.json({ success: true, token: "Bearer " + token });
+            }
+          );
+        } else {
+          //extra catch for errors? need this?
+          return res.status(401).json({
+            failed: "unauthorized no token"
           });
         }
-        return res.status(401).json({
-          failed: "unauthorized"
-        });
       });
     })
+    //catches other errors
     .catch(error => {
       res.status(500).json({
         error: error
