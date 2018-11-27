fbed707d51fda6777ebea453163d3ad821d45d2b
EMILYHLANE <embem80@gmail.com>
2018-11-27 12:19:04 -0800

    adds Post component shell to Home

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Header from "./Header";
+import Post from "./Post";
 
 function Home() {
   return (
     <div>
       <Header />
-      <h2>Home Page</h2>
+      <Post />
     </div>
   );
 }
