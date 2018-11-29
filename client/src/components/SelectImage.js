fff047d1f953e40d8ecf9739ca633cc1368ec0b3
EMILYHLANE <embem80@gmail.com>
2018-11-29 13:50:42 -0800

    adds private keys file and new reference to key to use on heroku

--- client/src/components/SelectImage.js
+++ client/src/components/SelectImage.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 import Image from "./Image";
 import axios from "axios";
-import { US_KEY } from "../config/keys";
-const KEY = process.env.REACT_APP_UNSPLASH_KEY || US_KEY.REACT_APP_UNSPLASH_KEY;
+const KEY =
+  process.env.REACT_APP_UNSPLASH_KEY ||
+  require("../config/keys").US_KEY.REACT_APP_UNSPLASH_KEY;
 
 class SelectImage extends Component {
   state = {
---
+++ client/src/config/keys.js
@@ -0,0 +1,10 @@
+export const US_KEY = {
+  REACT_APP_UNSPLASH_KEY:
+    process.env.REACT_APP_UNSPLASH_KEY ||
+    require("./privateKeys").US_KEY.REACT_APP_UNSPLASH_KEY
+};
+
+// export const US_SECRET = {
+//   UNSPLASH_SECRET:
+//     "d613a338c4f857b3c6cabac19a0b6c7828bd0c58c21fd7392c8833bdb9ec1655"
+// };
