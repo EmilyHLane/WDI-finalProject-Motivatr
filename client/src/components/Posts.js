f981277b95ba99619d8d116b2ddc6cdf5695426c
EMILYHLANE <embem80@gmail.com>
2018-11-28 18:08:57 -0800

    fixes issue where calls to api/post did not go to proxy localhost, fixed by not using nodemon

--- client/src/components/Posts.js
+++ client/src/components/Posts.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import Post from "./Post";
 import axios from "axios";
 
-// //get data from post api and pass to post as props
 class Posts extends Component {
   state = {
     posts: []
