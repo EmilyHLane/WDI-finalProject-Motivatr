0ca105e2f1f997e5a85cb23c3f4c94a477f35f60
EMILYHLANE <embem80@gmail.com>
2018-12-05 14:55:35 -0800

    adds nodemailer and test email in server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const dotenv = require("dotenv");
 dotenv.config();
 const express = require("express");
+const nodeMailer = require("nodemailer");
 const app = express();
 const port = process.env.PORT || 4000;
 const userRouter = require("./config/api/user/routes");
@@ -54,6 +55,35 @@ app.get("/", (req, res) => res.send("Hello World"));
 //------API endpoints------
 app.use("/api/user", userRouter);
 app.use("/api/post", postRouter);
+
+//------Send email testing------
+nodeMailer.createTestAccount((err, account) => {
+  const transporter = nodeMailer.createTransport({
+    host: "smtp.ethereal.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: "svgzjthrn3stjie3@ethereal.email",
+      pass: "cd6pPPHHPaemhbm5Wx"
+    }
+  });
+
+  let mailOptions = {
+    from: '"Emily Testing" <emtest@example.com>', // sender address
+    to: "fakeperson@example.com", // list of receivers
+    subject: "testing123", // Subject line
+    text: "hello world", // plain text body
+    html: "<b>Helloooooo World!</b>" // html body
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log("Message sent", info.messageId, info.response);
+    console.log("preview url: ", nodeMailer.getTestMessageUrl(info));
+  });
+});
 
 //------Catchall route handler------
 app.get("*", (req, res) => {
