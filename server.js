6151dedead608487c3d83f6129fb4a33f020ba96
EMILYHLANE <embem80@gmail.com>
2018-12-07 23:48:45 -0800

    adds authorization to acc cont allow headers

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
   next();
 });
