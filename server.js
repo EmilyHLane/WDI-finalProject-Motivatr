fd35c7876568ae2b5d2f3fc9ee933a344d94a42d
EMILYHLANE <embem80@gmail.com>
2018-12-07 23:40:12 -0800

    adds options method to access control allow methods

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use(passport.initialize());
 //------CORS------
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
